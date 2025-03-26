ccd4c6a78dacbb6ff8f78ebb63e6e67345ad4a23
cobbland <jacobdensford@proton.me>
2025-03-26 16:02:41 -0400

    Think through problem and make note

--- knight-moves.js
+++ knight-moves.js
@@ -5,13 +5,26 @@ function knightMoves(start, destination, moves = []) {
         return;
     }
     // Check if the knight has arrived.
-    if (start === destination) {
+    if (start[0] === destination[0] && start[1] === destination[1]) {
         youMadeIt(moves);
         return;
     }
 
     const nextMoves = possibleMoves(start);
-}
+    const updatedMoves = moves;
+    updatedMoves.push(start);
+    for (const move in nextMoves) {
+        // Check to make sure the move isn't already in updatedMoves
+        // (Maybe use that map method of array?
+        // See: https://www.reddit.com/r/learnjavascript/comments/xdp5ga/how_do_i_check_if_one_arrays_element_exist_in/)
+        // If it's not:
+        knightMoves(nextMoves[move], destination, updatedMoves);
+    }
+
+    return updatedMoves;
+} // Currently exceeds maximum call stack. Probably because the knight keeps 
+  // making the first possible move, which moves it away or moves it back to 
+  // where it just was???
 
 function inBounds(location) {
     let isInBounds = 0;
--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 import { knightMoves } from "./knight-moves.js";
 
-knightMoves([0, 7], [3, 7]);
+knightMoves([3, 4], [3, 7]);
